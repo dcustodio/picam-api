669bfb64933bc5589e02586e54e9103a7aa43dc8
David Custódio <davidlacustodio@gmail.com>
2018-09-30 12:12:47 +0200

    refactor: node-friendly code

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-import { takePicture, getPicture } from './routes/picture'
 const express = require('express')
 const bodyParser = require('body-parser')
-
 const app = express()
 const port = 3000
+const pictureRoute = require('./routes/picture')
 
 app.use(bodyParser.json())
 app.get('/', (req, res) => res.send('pong!'))
 
-app.post('/picture', takePicture)
-app.get('/picture/:captureId', getPicture)
+app.post('/picture', pictureRoute.takePicture)
+app.get('/picture/:captureId', pictureRoute.getPicture)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
