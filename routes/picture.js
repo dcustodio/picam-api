8328f88e58aeda5d28912e1f5d43dd62d6e5283a
David Custódio <davidlacustodio@gmail.com>
2018-09-30 12:13:13 +0200

    feat: upload pictures to cloudinary

--- routes/picture.js
+++ routes/picture.js
@@ -1,5 +1,10 @@
 const asyncHandler = require('express-async-handler')
 const raspberry = require('../src/raspberry')
+const fs = require('fs')
+const path = require('path')
+const appDir = path.dirname(require.main.filename)
+const config = require('../config.json')
+const cloudinary = require('cloudinary')
 
 export const takePicture = asyncHandler(async (req, res, next) => {
     const captureId = req.body.captureId
@@ -8,19 +13,51 @@ export const takePicture = asyncHandler(async (req, res, next) => {
         res.status(400).send('CaptureID required!')
     }
 
-    await raspberry.takePicture().then(function (result) {
-        console.log(req.params.captureId)
-        res.send(result)
-    }).catch(next) // Errors will be passed to Express.
+    const filePath = path.join(appDir, `pictures/pb-${captureId}.jpg`)
+
+    try {
+        await raspberry.takePicture().then(function (result) {
+            console.log(req.params.captureId)
+            res.send(result)
+        })
+
+        cloudinary.config({
+            cloud_name: config.cloudinaryCloudName,
+            api_key: config.cloudinaryApiKey,
+            api_secret: config.cloudinaryClientSecret
+        })
+
+        cloudinary.uploader.upload(filePath, function (result) { console.log(result) }, { public_id: captureId })
+    } catch (error) {
+        // Errors will be passed to Express.
+
+        next(error)
+    }
 })
 
 export const getPicture = asyncHandler(async (req, res, next) => {
-    // if (!req.params.captureId) {
-    //     res.status(400).send('Something broke!')
-    // }
+    if (!req.params.captureId) {
+        res.status(400).send('CaptureID required')
+    }
+    const captureId = req.params.captureId
 
-    // await raspberry.takePicture().then(function (result) {
-    //     console.log(req.params.captureId)
-    //     res.send(result)
-    // }).catch(next) // Errors will be passed to Express.
+    // fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
+    //     if (!err) {
+    //         console.log('received data: ' + data);
+
+    //         response.writeHead(200, {'Content-Type': 'text/html'});
+    //         response.write(data);
+    //         response.end();
+    //     } else {
+    //         console.log(err);
+    //     }
+    // });
+    try {
+        const filePath = path.join(appDir, `pictures/${captureId}.jpg`)
+        var contents = fs.readFileSync(filePath, 'utf8')
+
+        res.send(contents)
+    } catch (error) {
+        next(error)
+    }
 })
