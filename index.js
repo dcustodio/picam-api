b958ef6353ecf3d261232755b35bd84d32bd7118
David Custódio <davidlacustodio@gmail.com>
2018-10-01 20:11:16 +0000

    added cors

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 const app = express()
 const port = 3000
 const pictureRoute = require('./routes/picture')
 
 app.use(bodyParser.json())
+
+
+var corsOptions = {
+  origin: 'http://localhost:8080',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.options('*', cors(corsOptions))
+
 app.get('/', (req, res) => res.send('pong!'))
 
 app.post('/picture', pictureRoute.takePicture)
