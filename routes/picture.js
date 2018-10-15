d1bf6e15d8044706834b222469b03ce1e6b61919
David Custódio <davidlacustodio@gmail.com>
2018-10-15 21:47:26 +0200

    db persistance

--- routes/picture.js
+++ routes/picture.js
@@ -8,8 +8,21 @@ const cloudinary = require('cloudinary')
 const debug = require('debug')('picture')
 const Loki = require('lokijs')
 
-const db = new Loki('db.json')
-const photos = db.addCollection('photos')
+const db = new Loki('db.json',
+    {
+        autoload: true,
+        autoloadCallback: databaseInitialize,
+        autosave: true,
+        autosaveInterval: 4000
+    })
+
+function databaseInitialize () {
+    var photos = db.getCollection('photos')
+
+    if (photos === null) {
+        db.addCollection('photos')
+    }
+}
 
 cloudinary.config({
     cloud_name: config.cloudinaryCloudName,
@@ -18,6 +31,7 @@ cloudinary.config({
 })
 
 const takePicture = asyncHandler(async (req, res, next) => {
+    var photos = db.getCollection('photos')
     const captureId = req.body.captureId
 
     if (!captureId) {
@@ -66,6 +80,7 @@ const getPicture = asyncHandler(async (req, res, next) => {
     if (!req.params.captureId) {
         res.status(400).send('CaptureID required')
     }
+    var photos = db.getCollection('photos')
     const captureId = req.params.captureId
 
     // fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
@@ -85,13 +100,15 @@ const getPicture = asyncHandler(async (req, res, next) => {
         // const filePath = path.join(appDir, `pictures/${captureId}.jpg`)
         // var contents = fs.readFileSync(filePath, 'utf8')
 
-        res.send(result)
+        res.send(result[0] || {})
     } catch (error) {
         next(error)
     }
 })
 
 const getPictures = (req, res) => {
+    var photos = db.getCollection('photos')
+
     res.send(photos.data)
 }
 
